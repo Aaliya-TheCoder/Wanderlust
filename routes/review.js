608e69727b12750920c9f9949371a1da740d3b0a
Aaliy-TheCoder <aaliya969392@gmail.com>
2026-05-27 14:44:55 +0530

    Fix uppercase Models in review route

--- routes/review.js
+++ routes/review.js
@@ -3,7 +3,7 @@ const Review = require("../models/review.js");
 const router = express.Router({mergeParams: true});
 const wrapAsync= require("../utils/wrapAsync.js");
 const expressError = require("../utils/expresserror.js");
-const Listing = require("../Models/listing.js");
+const Listing = require("../models/listing.js");
 const {validatereview,isLoggedIn,isreviewAuthor} = require("../middleware.js");
 const reviewController = require("../controller/review.js");
 
