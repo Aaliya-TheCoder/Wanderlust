ba3005f2764dec82afd6d9dd2e7a288fb7ac7abd
Aaliy-TheCoder <aaliya969392@gmail.com>
2026-09-03 10:41:34 +0530

    Fix Middleware path error

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync.js");
 const passport= require("passport");
-const {saveRedirectUrl} = require("../middleware.js");
+const {saveRedirectUrl} = require("./middleware.js");
 const userController = require("../controller/user.js");
 router
 .route("/signup")
